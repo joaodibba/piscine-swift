3d088cb57b4a00cea5b9f3f626fb0ca8f6be67a3
João Miranda <113291032+joaodibba@users.noreply.github.com>
2026-02-20 19:18:45 +0000

    fix: update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   appType: "spa",
   plugins: [typeComposerPlugin()],
-  base: "./",
+  base: "/piscine-swift/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
